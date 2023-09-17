9a12edb89ae363bf7b90ddbf0f2b04c3ef5e2020
CardoPixel <123719219+CardoPixel@users.noreply.github.com>
2023-09-17 06:16:57 -0300

    feat: :sparkles: Added intial PWA Support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const path = require("path");
+
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+
+module.exports = nextConfig;
