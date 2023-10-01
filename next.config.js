33e59e6728186b7c40cef04abb89458c18c580c4
CardoPixel <cardo.pixelado@gmail.com>
2023-10-01 04:34:45 -0300

    refactor: :recycle: Total refactor [1/3]

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
+const withPWA = require('next-pwa')({
+  dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === 'development',
 });
 
-/** @type {import('next').NextConfig} */
-const path = require("path");
+const path = require('path');
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
